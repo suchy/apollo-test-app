c445a1861094350f4191ed2ab3648a9cc9cb61a4
Thanasis Polychronakis <thanpolas@gmail.com>
2018-03-14 13:49:23 +0000

    wire gql to express service

--- app/services/express.service.js
+++ app/services/express.service.js
@@ -1,12 +1,21 @@
 /**
  * @fileOverview The core express instance, requires all others.
  */
+const path = require('path');
+
+
 const BPromise = require('bluebird');
 const express = require('express');
 const bodyParser = require('body-parser');
-const nodeonExpressError = require('nodeon-express-error');
 const cors = require('cors');
-const { graphqlExpress } = require('apollo-server-express');
+const log = require('logg').getLogger('app.service.express');
+
+const {
+  graphqlExpress,
+  graphiqlExpress,
+} = require('apollo-server-express');
+
+// const graphqlHTTP = require('express-graphql');
 
 const webService = require('./web.service');
 const globals = require('../util/globals');
@@ -23,7 +32,7 @@ const expressService = module.exports = {};
  *
  * @return {BPromise} a promise.
  */
-expressService.prototype.init = BPromise.method(function () {
+expressService.init = BPromise.method(function () {
   /** @type {express} The express instance */
   expressService.app = express();
 
@@ -32,6 +41,13 @@ expressService.prototype.init = BPromise.method(function () {
 
   // remove x-powered-by header
   expressService.app.set('x-powered-by', false);
+
+  // Use pug for templates
+  expressService.app.engine('pug', require('pug').__express);
+  expressService.app.set('views',
+    path.join(__dirname, '/../../front/templates/'));
+  expressService.app.set('view engine', 'pug');
+
 
   // enable CORS
   const corsOptions = {
@@ -49,10 +65,25 @@ expressService.prototype.init = BPromise.method(function () {
 
   // GraphQL
   expressService.app.use('/graphql', bodyParser.json(),
-    graphqlExpress({ schema: myGraphQLSchema }));
+    graphqlExpress({
+      schema: myGraphQLSchema.schema,
+    }));
 
-  // nodeON Express Error Handler
-  expressService.app.use(nodeonExpressError);
+  expressService.app.use('/gqli', graphiqlExpress({
+    endpointURL: '/graphql',
+  }));
+
+  // Express Error Handler
+  expressService.app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+    log.warn('Express Error Handler :: error:', err.message);
+    if (!err.noStack) {
+      log.warn('Stack of express error:', err.stack);
+    }
+
+    const httpCode = err.httpCode || 500;
+
+    res.status(httpCode).json({ error: err.message });
+  });
 
   return webService.start(expressService.app);
 });
